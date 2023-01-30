ecb14c7ff0a2c49535cd05cbc08a08c77efb8aed
Giribabu Chitteni <Giribabu.Chitteni@einfochips.com>
2023-01-30 12:49:37 +0530

    modified main.tf errors will be resolve

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ terraform {
 
 provider "azurerm" {
   subscription_id = "9906489f-5645-44c1-a338-e48a8a7a21ba"
-  tenant_id = "b8c5386e-89b6-4572-be68-192298b572cf"
-  client_id = "f428b9fa-18b7-4756-82ea-35bcfb0a1d1b"
-  client_secret = "FrE8Q~dNnSG9SoN0VPKZYElh6D2MvG4WvB~xocOQ"
+ tenant_id = "b8c5386e-89b6-4572-be68-192298b572cf"
+ client_id = "f428b9fa-18b7-4756-82ea-35bcfb0a1d1b"
+ client_secret = "FrE8Q~dNnSG9SoN0VPKZYElh6D2MvG4WvB~xocOQ"
   features {}
 }
 
